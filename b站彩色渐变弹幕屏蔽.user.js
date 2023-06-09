ea01de8b05d197245b9abc68735289aac0833e16
hny3494317690 <84086594+hny3494317690@users.noreply.github.com>
2023-06-09 10:14:43 +0800

    Update b站彩色渐变弹幕屏蔽.user.js

--- b站彩色渐变弹幕屏蔽.user.js
+++ b站彩色渐变弹幕屏蔽.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         屏蔽B站彩色渐变弹幕
 // @namespace    https://github.com/hny3494317690/bili-vip-danmu-remove
-// @version      0.8
+// @version      0.85
 // @description  b站新出的彩色渐变弹幕太恶心了，用这个来屏蔽吧
 // @author       hny3494317690
 // @match        *://*.bilibili.com/*
@@ -17,5 +17,5 @@
   dom.style.visibility='hidden'
   dom.textContent="";
   dom.style.opacity=0;
-}}, 200)
+}}, 100)
 })();
